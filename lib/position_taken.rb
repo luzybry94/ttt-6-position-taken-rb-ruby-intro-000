0ab4d92dba75f2ec3b71761f45f45ae95540e4c2
Chaim Bryski <luzybry94@gmail.com>
2020-05-02 01:32:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,1 @@
 # code your #position_taken? method here!
-def position_taken?(array, index)
-  if (array[index] == " ")
-    return false
-  elsif (array[index] == "")
-    return false
-  elsif (array[index] == nil)
-    return false
-  else
-    return true
-  end
-end
